bfa3360d7280ec5ffab44e861cbb3ed7a9cebc28
Ben Churchill <bencbchurchill@gmail.com>
2015-09-27 20:53:18 -0600

    Added mask to Types and the type interactions

--- src/Formula.java
+++ src/Formula.java
@@ -6,6 +6,28 @@ import java.util.*;
 import java.lang.*;
 public class Formula
 {
+	/****** Keeps track of type interactions :: [attacking type][defending type] ******/
+	static double[][] effectiveness = new double[][]{
+			  //  			   0    1    2    3    4    5    6    7    8    9    10   11   12   13   14   15   16   17
+			  /*Normal*/  	{ 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 },
+			  /*Fighting*/  { 2.0, 1.0, 0.5, 0.5, 1.0, 2.0, 0.5, 0.0, 2.0, 1.0, 1.0, 1.0, 1.0, 0.5, 2.0, 1.0, 2.0, 0.5 },
+			  /*Flying*/  	{ 1.0, 2.0, 1.0, 1.0, 1.0, 0.5, 2.0, 1.0, 0.5, 1.0, 2.0, 2.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0 },
+			  /*Poison*/  	{ 1.0, 1.0, 1.0, 0.5, 0.5, 0.5, 1.0, 0.5, 0.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 },
+			  /*Ground*/  	{ 1.0, 1.0, 0.0, 2.0, 1.0, 2.0, 0.5, 1.0, 2.0, 2.0, 1.0, 0.5, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0 },
+			  /*Rock*/  	{ 1.0, 0.5, 2.0, 1.0, 0.5, 1.0, 2.0, 1.0, 0.5, 2.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0 },
+			  /*Bug*/  		{ 1.0, 0.5, 0.5, 0.5, 1.0, 1.0, 1.0, 0.5, 0.5, 0.5, 1.0, 2.0, 1.0, 2.0, 1.0, 1.0, 2.0, 0.5 },
+			  /*Ghost*/  	{ 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 0.5, 1.0 },
+			  /*Steel*/  	{ 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 0.5, 0.5, 0.5, 1.0, 0.5, 1.0, 2.0, 1.0, 1.0, 2.0 },
+			  /*Fire*/  	{ 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 2.0, 1.0, 2.0, 0.5, 0.5, 2.0, 1.0, 1.0, 2.0, 0.5, 1.0, 1.0 },
+			  /*Water*/  	{ 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 1.0, 1.0, 1.0, 2.0, 0.5, 0.5, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0 },
+			  /*Grass*/  	{ 1.0, 1.0, 0.5, 0.5, 2.0, 2.0, 0.5, 1.0, 0.5, 0.5, 2.0, 0.5, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0 },
+			  /*Electric*/  { 1.0, 1.0, 2.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 0.5, 0.5, 1.0, 1.0, 0.5, 1.0, 1.0 },
+			  /*Psychic*/   { 1.0, 2.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 0.0, 1.0 },
+			  /*Ice*/  	    { 1.0, 1.0, 2.0, 1.0, 2.0, 1.0, 1.0, 1.0, 0.5, 0.5, 0.5, 2.0, 1.0, 1.0, 0.5, 2.0, 1.0, 1.0 },
+			  /*Dragon*/  	{ 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 0.0 },
+			  /*Dark*/ 		{ 1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 0.5, 0.5 },
+			  /*Fairy*/  	{ 1.0, 2.0, 1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 0.5, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 1.0 }
+			    			};
     
 
     /****** Calculates the damage
@@ -33,7 +55,7 @@ public class Formula
      * @param pokemon b :: the defending pokemon
      * @param move m :: the move being used by pokemon a
      *****/
-    public static int modifier(Pokemon a, Pokemon b, Move m )
+    public static double modifier(Pokemon a, Pokemon b, Move m )
     {
         Random gen = new Random();
         
@@ -51,18 +73,13 @@ public class Formula
         
         //Calculate type modifier
         type = 1.0;
-        type *= dmgTypeMult(a.getType1(), m.getType());
-        type *= dmgTypeMult(a.getType2(), m.getType());
+        type *= effectiveness[m.getType().mask][b.getType1().mask];
+        type *= effectiveness[m.getType().mask][b.getType2().mask];
         
         //Calculate damage range
         roll = .85 + (1.0 - .85) * gen.nextDouble();
         
-        return (int)(stab * type * roll);
-    }
-    
-    public static double dmgTypeMult(Type pkmnType, Type moveType)
-    {
-    	
+        return (stab * type * roll);
     }
     
     
--- src/Type.java
+++ src/Type.java
@@ -1,22 +1,32 @@
 
 public enum Type 
 {
-	NORMAL, 
-	FIRE, 
-	FIGHTING, 
-	WATER, 
-	FLYING, 
-	GRASS, 
-	POISON, 
-	ELECTRIC, 
-	GROUND, 
-	PSYCHIC, 
-	ROCK, 
-	ICE, 
-	BUG, 
-	DRAGON, 
-	GHOST, 
-	DARK, 
-	STEEL, 
-	FAIRY
+	NORMAL(0), 
+	FIGHTING(1),
+	FLYING(2),
+	POISON(3),
+	GROUND(4),
+	ROCK(5),
+	BUG(6),
+	GHOST(7),
+	STEEL(8),
+	FIRE(9),
+	WATER(10),
+	GRASS(11),
+	ELECTRIC(12),
+	PSYCHIC(13),
+	ICE(14),
+	DRAGON(15),
+	DARK(16),
+	FAIRY(17);
+	
+	public final int mask;
+	Type(int mask)
+	{
+		this.mask=mask;
+	}
+	public int getMask()
+	{
+		return mask;
+	}
 }

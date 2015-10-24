51c9c9cd5e61640e2a4977382077b486e719ce1d
Bryan Powell <btp7272@gmail.com>
2015-10-24 14:26:42 -0600

    Created move list, changed move constructor

--- src/pokemonBattleSim/types/Move.java
+++ src/pokemonBattleSim/types/Move.java
@@ -5,7 +5,6 @@ public class Move
 	public String name;
 	public Type type = null;
 	public Attribute category = null;
-	public Contest contest = null;
 	public int pp;
 	public int power;
 	public double accuracy;
@@ -14,7 +13,6 @@ public class Move
 	private void setName(String name){this.name = name;}
 	private void setType(Type yype){this.type = type;}
 	private void setCategory(Attribute category){this.category = category;}
-	private void setContest(Contest contest){this.contest = contest;}
 	private void setPP(int pp){this.pp = pp;}
 	private void setPower(int power){this.power = power;}
 	private void setAccuracy(double accuracy){this.accuracy = accuracy;}
@@ -22,19 +20,17 @@ public class Move
 	public String getName(){return this.name;}
 	public Type getType(){return this.type;}
 	public Attribute getCategory(){return this.category;}
-	public Contest getContest(){return this.contest;}
 	public int getPP(){return this.pp;}
 	public int getPower(){return this.power;}
 	public double getAccuracy(){return this.accuracy;}
 	
 	
 	public Move(){}
-	public Move(String name, Type type, Attribute category, Contest contest, int pp, int power, double accuracy)
+	public Move(String name, Type type, Attribute category, int pp, int power, double accuracy)
 	{
 		this.name = name;
 		this.type = type;
 		this.category = category;
-		this.contest = contest;
 		this.pp = pp;
 		this.power = power;
 		this.accuracy = accuracy/100;

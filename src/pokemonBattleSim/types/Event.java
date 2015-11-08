674d51a27f9f9097e079c14c18f1c8b9a210de6b
shanealv <shane.alv@gmail.com>
2015-11-07 22:42:25 -0700

    Changes to use Interfaces

--- src/pokemonBattleSim/types/Event.java
+++ src/pokemonBattleSim/types/Event.java
@@ -2,7 +2,11 @@ package pokemonBattleSim.types;
 
 public class Event 
 {
+<<<<<<< HEAD
 	public static Boolean abilityEvent(IAbility ability, EventType type, IPokemon attacker, IPokemon defender, IField field, IPokemon attacker2, IPokemon defender2, Move lastMoveUsed)
+=======
+	public static Boolean abilityEvent(IAbility ability, EventType type, IPokemon wielder, IPokemon opponent, IField field, IPokemon attacker, IPokemon defender, Move lastMoveUsed)
+>>>>>>> refs/remotes/origin/master
 	{
 		if(ability.getEventTrigger().getMask() == type.getMask())
 		{

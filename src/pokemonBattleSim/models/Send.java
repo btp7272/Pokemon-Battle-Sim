ecab968815266ee2c362cfb3e83b5ebea4d67b35
Bryan Powell <btp7272@gmail.com>
2015-11-26 21:40:09 -0700

    Update

--- src/pokemonBattleSim/models/Send.java
+++ src/pokemonBattleSim/models/Send.java
@@ -6,25 +6,23 @@ import java.net.*;
 public class Send {
 	Socket opponent = null;  
     DataOutputStream output = null;
-    DataInputStream input = null;
     
 	public Socket createSocket()
 	{
 		try {
             opponent = new Socket("opponent", 8000);
             output = new DataOutputStream(opponent.getOutputStream());
-            input = new DataInputStream(opponent.getInputStream());
         } catch (UnknownHostException e) {
-            System.err.println("Don't know about host: hostname");
+            System.err.println("Don't know about host: opponent");
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to: hostname");
+            System.err.println("Couldn't get I/O for the connection to: opponent");
         }
 		return opponent;
 	}
 	
 	public void sendBytes(String s)
 	{
-		if (opponent!= null && output != null && input != null)
+		if (opponent!= null && output != null)
 		{
 			try	
 			{

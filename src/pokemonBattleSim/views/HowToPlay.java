6b6f04d03468627d715e9618fe04858e2a7db527
Bryan Powell <btp7272@gmail.com>
2015-12-01 14:01:45 -0700

    Added text to how to play

--- src/pokemonBattleSim/views/HowToPlay.java
+++ src/pokemonBattleSim/views/HowToPlay.java
@@ -13,6 +13,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.JEditorPane;
+import javax.swing.JTextPane;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class HowToPlay 
 {
@@ -44,9 +49,8 @@ public class HowToPlay
 	private void initialize() {
 		HTP = new JFrame();
 		HTP.setTitle("How To Play");
-		HTP.setBounds(100, 100, 560, 450);
+		HTP.setBounds(100, 100, 620, 508);
 		HTP.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		HTP.getContentPane().setLayout(null);
 		
 		JButton btnNewButton = new JButton("Return");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -55,8 +59,38 @@ public class HowToPlay
 				HTP.setVisible(false);
 			}
 		});
-		btnNewButton.setBounds(465, 399, 89, 23);
-		HTP.getContentPane().add(btnNewButton);
+		
+		JTextPane txtpnHe = new JTextPane();
+		txtpnHe.setText("To begin a game against the computer, press the \"Play VS AI\" button. "+
+		"This will open up the Team Builder, where you can select your team of six Pok\u00E9mon.\n\n"+
+		"You can give each Pok\u00E9mon four moves, one ability, and one held item per team member.\n\n"+
+		"Be careful, you cannot have any of the same two pokemon on your team, and no pokemon can learn the same move twice.\n\n"+
+		"After this, you have the option of changing your Individual Values and Effort Values, which are used when determining stats. The most common setup is already input by default.\n\n"+
+		"In addition, you have the options to choose each team member's nature and gender, and you can choose between level 50 or 100.\n\n"+
+		"\tWhen you are finished, press the \"Finalize\" button to begin your game.\n\n\n"+
+		"If you are going to host an online game, press the \"Host Online\" button. This will display your IP Address for another player to connect, and wait for a connection.\n"+
+		"If you are going to connect to an online game, press the \"Connect Online\" button, and type in the IP Address retrieved by the Hosting page from another player, and press \"Connect to Host.\"\n\n"+
+		"\t   After your game finishes, you will be returned to the main menu.\n\n\n"+
+		"Don't forget to view our Credits page where you can find the sources for all of our information and music!");
+		GroupLayout groupLayout = new GroupLayout(HTP.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(txtpnHe, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 608, Short.MAX_VALUE)
+						.addComponent(btnNewButton, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(txtpnHe, GroupLayout.PREFERRED_SIZE, 440, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+					.addGap(31))
+		);
+		HTP.getContentPane().setLayout(groupLayout);
 	}
-	
 }

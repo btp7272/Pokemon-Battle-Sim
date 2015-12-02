782f1cdf5f26d5e6308367b256fc124117cd4aac
Jacob Valdez <jacobbvaldez13@gmail.com>
2015-12-01 23:02:58 -0700

    GUI updates
    updates to credits and how to play

--- src/pokemonBattleSim/views/Credits.java
+++ src/pokemonBattleSim/views/Credits.java
@@ -56,13 +56,13 @@ public class Credits {
 	 */
 	private void initialize() {
 		Credits = new JFrame();
-		Credits.setTitle("Pokémon Rush");
-		Credits.setBounds(100, 100, 520, 560);
+		Credits.setTitle("Pok\u00E9mon Rush");
+		Credits.setBounds(100, 100, 545, 588);
 		Credits.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Credits.getContentPane().setLayout(null);
 		
 		JLabel lblCredits = new JLabel("Credits");
-		lblCredits.setBounds(204, 6, 71, 25);
+		lblCredits.setBounds(226, 9, 71, 25);
 		lblCredits.setFont(new Font("Tahoma", Font.BOLD, 20));
 		Credits.getContentPane().add(lblCredits);
 		
@@ -87,7 +87,7 @@ public class Credits {
 		Credits.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblSpecialThanks = new JLabel("Special Thanks");
-		lblSpecialThanks.setBounds(207, 183, 133, 15);
+		lblSpecialThanks.setBounds(204, 206, 133, 15);
 		lblSpecialThanks.setFont(new Font("Tahoma", Font.BOLD, 12));
 		Credits.getContentPane().add(lblSpecialThanks);
 		
@@ -95,7 +95,7 @@ public class Credits {
 		lblBulbapediabulbagardennet.setEditable(false);
 		lblBulbapediabulbagardennet.setHorizontalAlignment(SwingConstants.CENTER);
 		lblBulbapediabulbagardennet.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		lblBulbapediabulbagardennet.setBounds(10, 260, 243, 25);
+		lblBulbapediabulbagardennet.setBounds(10, 282, 243, 25);
 		Credits.getContentPane().add(lblBulbapediabulbagardennet);
 		
 		JButton btnNewButton = new JButton("Return");
@@ -105,14 +105,14 @@ public class Credits {
 				Credits.setVisible(false);
 			}
 		});
-		btnNewButton.setBounds(89, 465, 89, 23);
+		btnNewButton.setBounds(89, 487, 89, 23);
 		Credits.getContentPane().add(btnNewButton);
 		
 		txtHttpswwwyoutubecomuserglitchxcity = new JTextField();
 		txtHttpswwwyoutubecomuserglitchxcity.setEditable(false);
 		txtHttpswwwyoutubecomuserglitchxcity.setHorizontalAlignment(SwingConstants.CENTER);
 		txtHttpswwwyoutubecomuserglitchxcity.setText("youtube.com/user/GlitchxCity");
-		txtHttpswwwyoutubecomuserglitchxcity.setBounds(274, 260, 243, 25);
+		txtHttpswwwyoutubecomuserglitchxcity.setBounds(274, 282, 243, 25);
 		Credits.getContentPane().add(txtHttpswwwyoutubecomuserglitchxcity);
 		txtHttpswwwyoutubecomuserglitchxcity.setColumns(10);
 		
@@ -120,7 +120,7 @@ public class Credits {
 		txtHttpswwwyoutubecomuserpokeaimmd.setEditable(false);
 		txtHttpswwwyoutubecomuserpokeaimmd.setHorizontalAlignment(SwingConstants.CENTER);
 		txtHttpswwwyoutubecomuserpokeaimmd.setText("youtube.com/user/pokeaimMD");
-		txtHttpswwwyoutubecomuserpokeaimmd.setBounds(274, 373, 243, 25);
+		txtHttpswwwyoutubecomuserpokeaimmd.setBounds(274, 395, 243, 25);
 		Credits.getContentPane().add(txtHttpswwwyoutubecomuserpokeaimmd);
 		txtHttpswwwyoutubecomuserpokeaimmd.setColumns(10);
 		
@@ -128,7 +128,7 @@ public class Credits {
 		txtHttpswwwyoutubecomusermv.setEditable(false);
 		txtHttpswwwyoutubecomusermv.setHorizontalAlignment(SwingConstants.CENTER);
 		txtHttpswwwyoutubecomusermv.setText("youtube.com/user/3MV33");
-		txtHttpswwwyoutubecomusermv.setBounds(274, 462, 243, 25);
+		txtHttpswwwyoutubecomusermv.setBounds(274, 484, 243, 25);
 		Credits.getContentPane().add(txtHttpswwwyoutubecomusermv);
 		txtHttpswwwyoutubecomusermv.setColumns(10);
 		
@@ -136,77 +136,77 @@ public class Credits {
 		txtHttpwwwpkparaisocom.setEditable(false);
 		txtHttpwwwpkparaisocom.setHorizontalAlignment(SwingConstants.CENTER);
 		txtHttpwwwpkparaisocom.setText("pkparaiso.com");
-		txtHttpwwwpkparaisocom.setBounds(10, 404, 243, 25);
+		txtHttpwwwpkparaisocom.setBounds(10, 426, 243, 25);
 		Credits.getContentPane().add(txtHttpwwwpkparaisocom);
 		txtHttpwwwpkparaisocom.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Pok\u00E9mon Models:");
-		lblNewLabel_2.setBounds(78, 352, 175, 14);
+		lblNewLabel_2.setBounds(78, 374, 175, 14);
 		Credits.getContentPane().add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("Gameplay Input:");
 		lblNewLabel_3.setToolTipText("");
-		lblNewLabel_3.setBounds(340, 323, 102, 14);
+		lblNewLabel_3.setBounds(340, 345, 102, 14);
 		Credits.getContentPane().add(lblNewLabel_3);
 		
 		JLabel lblMusic = new JLabel("Music:");
-		lblMusic.setBounds(371, 210, 102, 14);
+		lblMusic.setBounds(371, 232, 102, 14);
 		Credits.getContentPane().add(lblMusic);
 		
 		JLabel lblMechanicsInformation = new JLabel("Battle Mechanics Information:");
-		lblMechanicsInformation.setBounds(53, 210, 188, 14);
+		lblMechanicsInformation.setBounds(53, 232, 188, 14);
 		Credits.getContentPane().add(lblMechanicsInformation);
 		
 		JLabel lblPkparaisocom = new JLabel("pkparaiso.com");
-		lblPkparaisocom.setBounds(10, 378, 123, 14);
+		lblPkparaisocom.setBounds(10, 400, 123, 14);
 		Credits.getContentPane().add(lblPkparaisocom);
 		
 		JLabel lblmv = new JLabel("3MV33");
-		lblmv.setBounds(274, 436, 123, 14);
+		lblmv.setBounds(274, 458, 123, 14);
 		Credits.getContentPane().add(lblmv);
 		
 		JLabel lblPokeaimmd = new JLabel("PokeaimMD");
-		lblPokeaimmd.setBounds(274, 349, 123, 14);
+		lblPokeaimmd.setBounds(274, 371, 123, 14);
 		Credits.getContentPane().add(lblPokeaimmd);
 		
 		JLabel lblGlitchxcity = new JLabel("GlitchxCity");
-		lblGlitchxcity.setBounds(274, 235, 102, 14);
+		lblGlitchxcity.setBounds(274, 257, 102, 14);
 		Credits.getContentPane().add(lblGlitchxcity);
 		
 		JLabel lblNewLabel_4 = new JLabel("Bulbapedia");
-		lblNewLabel_4.setBounds(10, 235, 179, 14);
+		lblNewLabel_4.setBounds(10, 257, 179, 14);
 		Credits.getContentPane().add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_5 = new JLabel("Smogon University");
-		lblNewLabel_5.setBounds(10, 296, 153, 14);
+		lblNewLabel_5.setBounds(10, 318, 153, 14);
 		Credits.getContentPane().add(lblNewLabel_5);
 		
 		txtAsdffdsa = new JTextField();
 		txtAsdffdsa.setEditable(false);
 		txtAsdffdsa.setHorizontalAlignment(SwingConstants.CENTER);
 		txtAsdffdsa.setText("smogon.com");
-		txtAsdffdsa.setBounds(10, 315, 243, 25);
+		txtAsdffdsa.setBounds(10, 337, 243, 25);
 		Credits.getContentPane().add(txtAsdffdsa);
 		txtAsdffdsa.setColumns(10);
 		
 		JLabel lblNewLabel_6 = new JLabel("This is a non-profit fan-made game.");
-		lblNewLabel_6.setBounds(274, 38, 270, 14);
+		lblNewLabel_6.setBounds(274, 65, 270, 14);
 		Credits.getContentPane().add(lblNewLabel_6);
 		
 		JLabel lblAsdf = new JLabel("Intelectual property of ");
-		lblAsdf.setBounds(274, 63, 270, 14);
+		lblAsdf.setBounds(274, 90, 270, 14);
 		Credits.getContentPane().add(lblAsdf);
 		
 		JLabel lblNewLabel_7 = new JLabel("Pok\u00E9mon\u2122 is owned by Nintendo,");
-		lblNewLabel_7.setBounds(274, 87, 270, 14);
+		lblNewLabel_7.setBounds(274, 114, 270, 14);
 		Credits.getContentPane().add(lblNewLabel_7);
 		
 		JLabel lblPleaseSupportThe = new JLabel("Game Freak, and Creatures Inc.");
-		lblPleaseSupportThe.setBounds(274, 111, 270, 14);
+		lblPleaseSupportThe.setBounds(274, 138, 270, 14);
 		Credits.getContentPane().add(lblPleaseSupportThe);
 		
 		JLabel lblNewLabel_8 = new JLabel("Please support the officail release.");
-		lblNewLabel_8.setBounds(274, 135, 270, 14);
+		lblNewLabel_8.setBounds(274, 162, 270, 14);
 		Credits.getContentPane().add(lblNewLabel_8);
 		
 		txtTwittercomjoeypokeaim = new JTextField();
@@ -214,7 +214,7 @@ public class Credits {
 		txtTwittercomjoeypokeaim.setHorizontalAlignment(SwingConstants.CENTER);
 		txtTwittercomjoeypokeaim.setEditable(false);
 		txtTwittercomjoeypokeaim.setColumns(10);
-		txtTwittercomjoeypokeaim.setBounds(274, 399, 243, 25);
+		txtTwittercomjoeypokeaim.setBounds(274, 421, 243, 25);
 		Credits.getContentPane().add(txtTwittercomjoeypokeaim);
 		
 		txtTwittercomglitchxcity = new JTextField();
@@ -222,7 +222,7 @@ public class Credits {
 		txtTwittercomglitchxcity.setHorizontalAlignment(SwingConstants.CENTER);
 		txtTwittercomglitchxcity.setEditable(false);
 		txtTwittercomglitchxcity.setColumns(10);
-		txtTwittercomglitchxcity.setBounds(274, 286, 243, 25);
+		txtTwittercomglitchxcity.setBounds(274, 308, 243, 25);
 		Credits.getContentPane().add(txtTwittercomglitchxcity);
 		
 		txtTwittercommv = new JTextField();
@@ -230,7 +230,7 @@ public class Credits {
 		txtTwittercommv.setHorizontalAlignment(SwingConstants.CENTER);
 		txtTwittercommv.setEditable(false);
 		txtTwittercommv.setColumns(10);
-		txtTwittercommv.setBounds(274, 485, 243, 25);
+		txtTwittercommv.setBounds(274, 507, 243, 25);
 		Credits.getContentPane().add(txtTwittercommv);
 		
 		txtTwittercomsirskaro = new JTextField();
@@ -243,7 +243,7 @@ public class Credits {
 		
 		JLabel lblFeedback = new JLabel("Feedback");
 		lblFeedback.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblFeedback.setBounds(103, 139, 111, 15);
+		lblFeedback.setBounds(103, 144, 111, 15);
 		Credits.getContentPane().add(lblFeedback);
 		
 		txtPokemonrushfeedbackgmailcom = new JTextField();
@@ -251,7 +251,7 @@ public class Credits {
 		txtPokemonrushfeedbackgmailcom.setHorizontalAlignment(SwingConstants.CENTER);
 		txtPokemonrushfeedbackgmailcom.setEditable(false);
 		txtPokemonrushfeedbackgmailcom.setColumns(10);
-		txtPokemonrushfeedbackgmailcom.setBounds(6, 152, 247, 25);
+		txtPokemonrushfeedbackgmailcom.setBounds(10, 170, 247, 25);
 		Credits.getContentPane().add(txtPokemonrushfeedbackgmailcom);
 	}
 }
